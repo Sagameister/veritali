e95ee0b06f7fe0c9a14541e61516f4a43dc499ca
Sagameister <86540738+Sagameister@users.noreply.github.com>
2026-07-06 03:30:30 +0200

    Deregister legacy listing schema in Sanity Studio to clean up sidebar dashboard

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,6 +1,5 @@
 import { bilingual } from "./bilingual";
 import { bilingualText } from "./bilingualText";
-import { listing } from "./listing";
 import { news } from "./news";
 
-export const schemaTypes = [bilingual, bilingualText, listing, news];
+export const schemaTypes = [bilingual, bilingualText, news];
